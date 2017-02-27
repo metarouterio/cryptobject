cc70028447c55d5521a97d1ae7e1cdc61d6356ea
Mike Perry <mike@astronomer.io>
2017-02-27 18:13:59 -0500

    can now specify paths to ignore by passing an array of string paths as the last parameter to encrypt or decrypt

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
 import CryptoJS from "crypto-js";
 import traverse from "traverse";
-import curry from "curry";
 
-export function encrypt(passphrase, obj) {
+/**
+ * traverses an object, encrypting each value of string type
+ * using the passphrase
+ * @param passphrase the encryption key
+ * @param obj the object to encrypt
+ * @param ignore a string array of paths to not encrypt
+ */
+export function encrypt(passphrase, obj, ignore=[]) {
     return traverse(obj).map(function(item) {
-        if (typeof item === "string" && item !== "") {
+        const path = this.path.join('.');
+        const encrypt = !ignore.includes(path);
+        if (encrypt && typeof item === "string" && item !== "") {
             this.update(CryptoJS.AES.encrypt(item, passphrase).toString());
         }
     });
 }
 
-export function decrypt(passphrase, obj) {
+/**
+ * traverses an object, decrypting each value of string type
+ * using the passphrase
+ * @param passphrase the key used to originally encrypt
+ * @param obj the object to decrypt
+ * @param ignore a string array of paths to not decrypt
+ */
+export function decrypt(passphrase, obj, ignore=[]) {
     return traverse(obj).map(function(item) {
-        if (typeof item === "string" && item !== "") {
+        const path = this.path.join('.');
+        const decrypt = !ignore.includes(path);
+        if (decrypt && typeof item === "string" && item !== "") {
             this.update(CryptoJS.AES.decrypt(item, passphrase).toString(CryptoJS.enc.Utf8));
         }
     });
@@ -20,10 +37,12 @@ export function decrypt(passphrase, obj) {
 
 export default {
     encryptor(passphrase) {
-        return curry(encrypt)(passphrase);
+        // partially apply encrypt with passphrase
+        return encrypt.bind(null, passphrase);
     },
 
     decryptor(passphrase) {
-        return curry(decrypt)(passphrase);
+        // partially apply decrypt with passphrase
+        return decrypt.bind(null, passphrase);
     }
 };
--- test/index.js
+++ test/index.js
@@ -6,23 +6,57 @@ describe('cryptobject', function() {
     const passphrase = 'testpass';
     const encrypt = encryptor(passphrase);
     const decrypt = decryptor(passphrase);
-    const obj = {
-        key: 'value'
-    };
 
     describe('#encryptor', function() {
         it('encrypts string values in an object', function() {
+            const obj = {
+                key: 'value'
+            };
             const encryptedObj = encrypt(obj);
             assert(encryptedObj.key !== 'value', 'value should change after encryption');
-            assert(encryptedObj.key.length > obj.key.length, 'Encrypted value length should be greater than length of original value');
+            assert(encryptedObj.key.length > obj.key.length, 'encrypted value length should be greater than length of original value');
+        });
+
+        it('does not encrypt fields specified in ignore', function() {
+            const obj = { key: 'value', ignore: 'ignored' };
+            const ignore = ['ignore'];
+            const encryptedObj = encrypt(obj, ignore);
+            assert(encryptedObj.ignore === 'ignored', 'should ignore fields passed in via array');
+        });
+
+        it('ignores nested paths with dot syntax', function() {
+            const obj = {
+                key: 'value',
+                child: {
+                    ignore: 'ignored'
+                }
+            };
+            const ignore = ['child.ignore'];
+            const encryptedObj = encrypt(obj, ignore);
+            assert(encryptedObj.child.ignore === 'ignored', 'should ignore nested paths provided via ignore');
         });
     });
 
     describe('#decryptor', function() {
         it('decrypts an object', function() {
+            const obj = {
+                key: 'value'
+            };
             const encryptedObj = encrypt(obj);
             const decryptedObj = decrypt(encryptedObj);
             assert(decryptedObj.key === 'value', 'decrypt should get the original value back');
+        });
+
+        it('does not decrypt an ignored field', function() {
+            const obj = {
+                key: 'value',
+                child: {
+                    ignore: 'ignored'
+                }
+            };
+            const ignore = ['key', 'child.ignore'];
+            const decryptedObj = decrypt(obj, ignore);
+            assert.deepEqual(obj, decryptedObj);
         });
     });
 });
